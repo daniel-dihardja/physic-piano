96b480727a157542b46f42b8e6aca020b213149d
Daniel Dihardja <daniel@dihardja.de>
2022-03-07 14:48:59 +0100

    add web worker for piano_genie

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import {Piano} from "./piano";
-import {Block, Mouse} from './classes';
+import {Block, Mouse, Ball} from './classes';
 
 let blockA;
 let blockB;
 let ball;
 let ground;
 let mouse;
+let wallLeft;
+let wallRight;
+let wallTop;
+
+
+const worker = new Worker('worker.js');
+
+worker.onmessage = (event) => {
+  const {note} = event.data;
+  console.log(note);
+}
 
 const sketch = (s) => {
   s.setup = async () => {
@@ -16,22 +27,59 @@ const sketch = (s) => {
     let world = engine.world;
 
     // create two boxes and a ground
-    blockA = new Block(s, world, { x: 200, y: 200, w: 80, h: 80, color: 'white' });
-    blockB = new Block(s, world, { x: 270, y: 50, w: 160, h: 80, color: 'white' });
+    blockA = new Ball(s, world, { x: 200, y: 200, r: 30, color: 'white' }, {restitution: 1});
+    blockB = new Ball(s, world, { x: 270, y: 50, r: 30, color: 'white' }, {restitution: 1});
     ground = new Block(s, world, { x: 400, y: 500, w: 810, h: 15, color: 'grey' }, { isStatic: true, angle: s.PI/36 });
+
+    wallLeft = new Block(s, world, {x: -50, y: 300, h: 600, w: 100, color: 'grey'}, {isStatic: true});
+    wallRight = new Block(s, world, {x: 850, y: 300, h: 600, w: 100, color: 'grey'}, {isStatic: true});
+    wallTop = new Block(s, world, {x: 400, y: -50, h: 100, w: 1000, color: 'grey'}, {isStatic: true});
 
     // add a mouse to manipulate Matter objects
     mouse = new Mouse(s, engine, canvas, { stroke: 'magenta', strokeWeight: 2 });
+    mouse.on('mousedown', () => {
+      shakeScene(engine);
+    });
+
+    Matter.Events.on(engine, 'collisionStart', s.onCollisionStart);
 
     // run the engine
     Matter.Runner.run(engine);
   }
+
+  s.onCollisionStart = (event) => {
+    const pairs = event.pairs;
+
+    // change object colours to show those starting a collision
+    for (let i = 0; i < pairs.length; i++) {
+      const pair = pairs[i];
+      worker.postMessage({button: Math.floor(Math.random() * 8)});
+    }
+  },
 
   s.draw = () => {
     s.background('black');
     blockA.draw();
     blockB.draw();
     ground.draw();
+    wallTop.draw();
   }
 }
+
+function shakeScene(engine) {
+  const bodies = Matter.Composite.allBodies(engine.world);
+
+  for (let i = 0; i < bodies.length; i++) {
+    const body = bodies[i];
+    if (!body.isStatic && body.position.y >= 200) {
+      const forceMagnitude = 0.02 * body.mass;
+
+      Matter.Body.applyForce(body, body.position, {
+        x: (forceMagnitude + Matter.Common.random() * forceMagnitude) * Matter.Common.choose([1, -1]),
+        y: -forceMagnitude + Matter.Common.random() * -forceMagnitude
+      });
+    }
+  }
+}
+
 new p5(sketch);
---
+++ worker.js
@@ -0,0 +1,20 @@
+importScripts('./node_modules/@tensorflow/tfjs/dist/tf.min.js');
+importScripts('./node_modules/@magenta/music/es6/core.js');
+importScripts('./node_modules/@magenta/music/es6/piano_genie.js');
+
+const genie = new piano_genie.PianoGenie('./checkpoint');
+const whiteKeys = [...Array(88).keys()];
+
+(async function init() {
+  await genie.initialize();
+  genie.nextFromKeyWhitelist(0, whiteKeys, 0.5);
+  genie.resetState();
+  console.log('piano genie is ready');
+})();
+
+// Main script asks for work.
+self.onmessage = async (e) => {
+  const {button} = e.data;
+  const note = genie.nextFromKeyList(button, whiteKeys, 0.5);
+  postMessage({note});
+};
